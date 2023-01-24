5c20b9771737ff157bc59c97699e56e0f826a5ba
Oksana <ksenia79460@gmail.com>
2023-01-24 16:52:08 +0000

    create and styled navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/"></Route>
+          <Route path="/cart"></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
